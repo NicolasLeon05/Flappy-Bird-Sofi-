9d9045267964042615d75fcb95ace7370f96e6e5
Nicolas Leon <nicolasaleon05@gmail.com>
2024-11-18 21:54:16 -0300

    Update objectives

--- FlappyBird/src/main.cpp
+++ FlappyBird/src/main.cpp
@@ -1,12 +1,9 @@
 #include "objects/game.h"
 
 //Agregar botones para mutear sfx y bgm
-//Agregar puntaje
-//Completar pagina de git
+//Completar pagina de git e itch
 //Completar archivos .bat
-//Agregar pantalla de result, pantalla de pausa y pantalla de how to play
 //Quizas hacer que los obstaculos se muevan de arriba a abajo una vez alcanzado cierto score
-//Completar los creditos con los sonidos
 //Añadir textura a los obstaculos
 
 int main()
