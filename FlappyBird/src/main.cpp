bbeb5724414d01e6c6a1fd0087452394083637fe
Nicolas Leon <nicolasaleon05@gmail.com>
2024-11-20 05:47:15 -0300

    Final objectives update

--- FlappyBird/src/main.cpp
+++ FlappyBird/src/main.cpp
@@ -1,9 +1,5 @@
 #include "objects/game.h"
 
-//Completar pagina de git e itch
-//Quizas hacer que los obstaculos se muevan de arriba a abajo una vez alcanzado cierto score
-//Agregar fonts a los creditos
-
 int main()
 {
 	FlappyBird::Play();
